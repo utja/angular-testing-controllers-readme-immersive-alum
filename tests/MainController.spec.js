7f06af864a1659c0a6427faadbbb825f9f23f442
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-12-07 17:56:48 -0500

    Done.

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,18 @@
 describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
-    });
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+
+  it('should have Stebe Jobs name', function() {
+    var $scope = {};
+
+    var controller = $controller('MainController', { $scope: $scope});
+
+    expect(controller.name).toEqual('Steve Jobs');
+  });
 });
